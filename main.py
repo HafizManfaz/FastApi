32c2c1e4fd2ddabb014d3d624b6dc5265830e795
Hafiz Manfaz Ali <manfaz050@gmail.com>
2025-08-11 23:47:39 +0500

    Load data from json is implemented to the api

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get('/home')
 def welcome_home():
     return {'message':'Welcome to Home'}
+
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+        return data
+    
